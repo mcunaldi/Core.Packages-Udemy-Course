26d4d6fe2f6aa7f419cb295e30cdb89d32b7cbb1
Mehmet Can ÜNALDI <mehmetcanunaldi@gmail.com>
2024-03-25 21:47:38 +0300

    Pipeline İmplementasyonu - Validation & Transaction

--- Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/ExceptionHandler.cs
@@ -7,9 +7,11 @@ public abstract class ExceptionHandler
         exception switch
         {
             BusinessException businessException => HandleException(businessException),
+            ValidationException validationException => HandleException(validationException),
             _ => HandleException(exception)
         };
 
     protected abstract Task HandleException(BusinessException businessException);
+    protected abstract Task HandleException(ValidationException businessException);
     protected abstract Task HandleException(Exception businessException);
 }
